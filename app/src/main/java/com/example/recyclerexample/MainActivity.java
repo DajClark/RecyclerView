94e3358a2e9311181e1e5eef4d251abe9b44558f
Daniel Clark <dajclark@googlemail.com>
2018-11-19 23:16:22 +0000

    Added controller class

--- app/src/main/java/com/example/recyclerexample/MainActivity.java
+++ app/src/main/java/com/example/recyclerexample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.recyclerexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment todoListFragment = fm.findFragmentById(R.id.fragment_container);
+        if (todoListFragment == null){
+            todoListFragment = new TodoListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, todoListFragment)
+                    .commit();
+        }
+
     }
+
 }
